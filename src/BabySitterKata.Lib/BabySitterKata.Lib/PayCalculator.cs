c870ba21bfdaafd9b68d5d463f159a68768a405a
Jacob <jjjlalonde@gmail>
2018-09-22 11:05:27 -0400

    Initial test for Pay Calculation

---
+++ src/BabySitterKata.Lib/BabySitterKata.Lib/PayCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BabySitterKata.Lib
+{
+    public class PayCalculator
+    {
+        public int CalculatePayForNumberOfHours(TimeSpan duration)
+        {
+            return duration.Hours * 12;
+        }
+    }
+}
---
+++ src/BabySitterKata.Lib/BabySitterKata.Tests/PayCalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using BabySitterKata.Lib;
+using NUnit.Framework;
+using System;
+
+namespace BabySitterKata.Tests
+{
+    [TestFixture]
+    public class PayCalculatorTests
+    { 
+        [Test]
+        public void CalculateBasePayForSetNumberOfHours()
+        {
+            var payCalculator = new PayCalculator();
+            Assert.AreEqual(48, payCalculator.CalculatePayForNumberOfHours(new TimeSpan(4, 0 , 0)));
+        }
+    }
+}
