00b095b66f39045b49bc22ff8ba584ad9db774ec
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2018-09-24 17:43:13 -0400

    Added sanity test for the service facade, left a note to why I didn't test it more

---
+++ src/BabySitterKata.Lib/BabySitterKata.Lib/BabySittingFacade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BabySitterKata.Lib
+{
+    public class BabySittingFacade
+    {
+        private readonly HourValidator _hourValidator;
+        private readonly PayCalculator _payCalculator;
+
+        public BabySittingFacade()
+        {
+            _hourValidator = new HourValidator();
+            _payCalculator = new PayCalculator();
+        }
+        public int ValidateAndCalculateBabySittingExpense(DateTime startTime, DateTime endTime)
+        {
+            _hourValidator.ValidateTimesAreAcceptable(startTime, endTime);
+            return _payCalculator.CalculatePayForNumberOfHours(startTime, endTime);
+        }
+    }
+}
---
+++ src/BabySitterKata.Lib/BabySitterKata.Tests/BabySitterFacadeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BabySitterKata.Lib;
+using NUnit.Framework;
+
+namespace BabySitterKata.Tests
+{
+    [TestFixture]
+    public class BabySitterFacadeTests
+    {
+        [Test]
+        /*
+         * This is really a sanity check to show I thought about an orchestrator
+         * And didn't just build random components
+         * But we only need one test to show it calls the correct components, not checking their actual logic.
+         */
+        public void BabySittingFromSixPmToEightPmValidAndReturnsProperCost()
+        {
+            var babySittingFacade = new BabySittingFacade();
+            var startTime = new DateTime(2000, 1, 1, 18, 0, 0);
+            var endTime = new DateTime(2000, 1, 1, 20, 0, 0);
+            Assert.DoesNotThrow(() => babySittingFacade.ValidateAndCalculateBabySittingExpense(endTime, startTime));
+            Assert.AreEqual(24, babySittingFacade.ValidateAndCalculateBabySittingExpense(startTime, endTime));
+        }
+    }
+}
